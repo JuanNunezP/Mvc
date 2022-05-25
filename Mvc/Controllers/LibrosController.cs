62e2812b629798b9ac78496e14b7addb45bc0bfe
Juan Pablo <jnunezpinilla@gmail.com>
2022-05-25 00:12:04 -0500

    Add project files.

---
+++ Mvc/Controllers/LibrosController.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mvc.Data;
+using Mvc.Models;
+using System.Collections.Generic;
+
+namespace Mvc.Controllers
+{
+    public class LibrosController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public LibrosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+          //HTTP GET  INDEX
+        public IActionResult Index()
+        {
+
+            IEnumerable<Libro> listLibros = _context.Libro;
+            return View(listLibros);
+        }
+
+        //HTTP Get Create
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        //HTTP Post Create
+        [HttpPost]
+        public IActionResult Create(Libro libro)
+
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Libro.Add(libro);
+                _context.SaveChanges();
+
+
+                TempData["mensaje"] = "El se ha creado correctamente";
+                return RedirectToAction("Index");    
+            }
+
+            return View();
+        }
+
+        //Http Get Edit
+
+        public IActionResult Edit(int? id) {
+
+            if (id == null || id == 0) 
+            {
+                return NotFound();
+                
+            }
+            //Obtener el libro
+            var libro = _context.Libro.Find(id);
+
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+
+              return View(libro);
+        }
+
+        //HTTP Post Edit
+        [HttpPost]
+        public IActionResult Edit(Libro libro)
+
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Libro.Update(libro);
+                _context.SaveChanges();
+
+
+                TempData["mensaje"] = "El libro se ha editado correctamente";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        //HTTP GET delete
+
+        public IActionResult Delete(int? id)
+        {
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+
+            }
+            //Obtener el libro
+            var libro = _context.Libro.Find(id);
+
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+
+            return View(libro);
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]  
+        public IActionResult DeleteLibro(int? id) {
+            var libro = _context.Libro.Find(id);    
+            if (libro == null) 
+            {
+                return NotFound();
+            }
+
+
+            _context.Libro.Remove(libro);
+            _context.SaveChanges();
+
+
+            TempData["mensaje"] = "El libro se ha Eliminado correctamente";
+
+            return View("Index");
+        }
+         
+    }
+}
---
+++ Mvc/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Mvc.Models;
+using Microsoft.EntityFrameworkCore;
+//using MySQL.Data.EntityFrameworkCore.Extensions;
+
+
+namespace Mvc.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Libro> Libro { get; set; }
+    }
+}
